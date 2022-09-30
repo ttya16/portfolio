7c3b9a74bcfc5c6bd74cb46ca0a864f866ec5008
ttya16 <drpepperkenny@gmail.com>
2022-10-01 00:58:02 +0900

    fixed skill icon order

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -56,7 +56,7 @@ const Header = () => {
         whileInView={scaleVariants.whileInView}
         className="app__header-circles"
       >
-        {[images.python, images.react, images.typescript].map((circle, index) => (
+        {[images.react, images.python, images.typescript].map((circle, index) => (
           <div className="circle-cmp app__flex" key={circle.index}>
             <img src={circle} alt="circle" />
           </div>
